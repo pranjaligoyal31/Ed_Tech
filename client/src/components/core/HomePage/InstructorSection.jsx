0c02d069164093c8b0236b003464b8868191498f
pranjaligoyal31 <goyalpranjali09@gmail.com>
2025-05-26 20:25:29 +0530

    added some component files

---
+++ client/src/components/core/HomePage/InstructorSection.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Instructor from '../../../assets/Images/teacher3.png'
+import HighlightText from './HighlightText'
+import CTAButton from "../HomePage/Button"
+import { FaArrowRight } from 'react-icons/fa'
+
+const InstructorSection = () => {
+    return (
+        <div className='mt-16'>
+            <div className='flex flex-col-reverse lg:flex-row gap-10 lg:gap-20 items-center'>
+                <div className='w-[50%]'>
+                    <img
+            src={Instructor}
+            alt="Instructor"
+            className='shadow-white rounded-3xl'
+          />
+                </div>
+                <div className='lg:w-[50%] flex flex-col'>
+          <div className='text-3xl lg:text-4xl font-semobold w-[50%] mb-2'>
+            Become an
+            <HighlightText text={"Instructor"} />
+          </div>
+
+          <p className='font-medium text-[16px] w-[80%] text-richblack-300 mb-12'>
+            Instructors from around the world teach millions of students on StudyNotion. We provide the tools and skills to teach what you love.
+          </p>
+
+         <div className='w-fit'>
+            <CTAButton active={true} linkto={"/signup"}>
+              <div className='flex flex-row gap-2 items-center'>
+                Start Learning Today
+                <FaArrowRight />
+              </div>
+            </CTAButton>
+          </div>
+        </div>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import HighlightText from '../components/core/HomePage/HighlightText';
-import CTAButton from '../components/core/HomePage/CTAButton';
+import CTAButton from '../components/core/HomePage/Button';
 import banner from '../assets/banner.mp4'
 import CodeBlocks from '../components/core/HomePage/CodeBlocks';
+import TimelineSection from '../components/core/HomePage/TimelineSection'
+import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection'
+import InstructorSection from '../components/core/HomePage/InstructorSection'
 
 const Home = () => {
   return (
@@ -114,8 +117,66 @@ const Home = () => {
 
         </div>
       {/* Section 2 */}
+  <div className='bg-pure-greys-5 text-richblack-700'>
+    <div className='homepage_bg h-[333px]'>
+        <div className='w-11/12 max-w-maxContent flex flex-col items-center gap-5 mx-auto'>
 
+        <div className='h-[150px]'></div>
+
+        <div className='flex flex-row gap-7 text-white'>
+            <CTAButton active={true} linkto={"/signup"}>
+                <div>
+                    Explore Full Catalog
+                    <FaArrowRight />
+                </div>
+            </CTAButton>
+            <CTAButton active={false} linkto={"/signup"}>
+                <div>
+                    Learn more
+                    
+                </div>
+            </CTAButton>
+        </div>
+
+        </div>
+    </div>
+  <div className='mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7'>
+  <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
+    <div className='text-4xl font-semibold w-[45%]'>
+        Get the Skills you need for 
+        <HighlightText text={"Job that is in demand"}/>
+    </div>
+
+    <div className='flex flex-col gap-10 w-[40%]'>
+        <div className='text-[16px]'>
+            The modern StudyNotion is the dictates its own terms. Today, to be a competitive
+            specialist requires more than professional skills.
+        </div>
+        <CTAButton active={true} linkto={"/signup"}>
+            <div>
+                Learn more
+            </div>
+        </CTAButton>
+
+    </div>
+  </div>
+
+  <TimelineSection />
+
+   <LearningLanguageSection />
+
+  </div>
+   
+
+  </div>
       {/* Section 3 */}
+      <div className='w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between gap-8
+       first-letter bg-richblack-900 text-white'>
+        <InstructorSection />
+
+        <h2 className='text-center text-4xl font-semibold mt-10'>review from other learners</h2>
+        {/* Review slider  */}
+       </div>
       {/* Footer */}
     </div>
   )
