3fc2af05d620545e07447d1f792c8ed8ad1b6925
pranjaligoyal31 <goyalpranjali09@gmail.com>
2025-05-26 23:50:36 +0530

    some changes in exploremore

--- client/src/components/core/HomePage/ExploreMore.jsx
+++ client/src/components/core/HomePage/ExploreMore.jsx
@@ -25,16 +25,54 @@ const ExploreMore = () => {
 
   return (
     <div>
-        <div>
+        
         <div className="text-3xl lg:text-4xl font-semibold text-center my-10">
             Unlock the
           <HighlightText text={"Power of Code"} />
+          </div>
           <p className="text-center text-richblack-300 text-base lg:text-lg font-semibold mt-1">
             Learn to Build Anything You Can Imagine
           </p>
+
+          <div className="mt-5 flex flex-row rounded-full bg-richblack-800 mb-5 border-richblack-100
+          px-1 py-1">
+            {
+                tabsName.map((element, index) => {
+                    return (
+                        <div
+              className={` text-[16px] flex flex-row items-center gap-2 ${currentTab === element
+                ? "bg-richblack-900 text-richblack-5 font-medium"
+                : "text-richblack-200"
+                } px-7 py-[7px] rounded-full transition-all duration-200 cursor-pointer hover:bg-richblack-900 hover:text-richblack-5`}
+              key={index}
+              onClick={() => setMyCards(element)}
+            >
+              {element}
+            </div>
+                    )
+                })
+            }
+          </div>
+          <div className="lg:h=[150px"></div>
+            {/*course card ka group*/}
+            <div className="absolute flex flex-row gap-10 justify-between w-full">
+                {
+                    courses.map((element, index) => {
+          return (
+            <CourseCard
+              key={index}
+              cardData={element}
+              currentCard={currentCard}
+              setCurrentCard={setCurrentCard}
+            />
+          )
+        })
+                }
+          </div>
+
+
         </div>
-    </div>
-    </div>
+    
   )
 }
 
